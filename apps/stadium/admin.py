2949aac884ae59312a2da8a9530514a3124a0e56
Bamdad <bamdadmeh@yahoo.com>
2022-09-10 21:48:51 +0430

    StadiumModel and SeatModel registered in Django admin

--- apps/stadium/admin.py
+++ apps/stadium/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.stadium.models import Stadium, Seat
+
+
+class SeatInline(admin.StackedInline):
+
+    model = Seat
+    extra = 1
+
+
+@admin.register(Stadium)
+class StadiumAdmin(admin.ModelAdmin):
+
+    inlines = (SeatInline,)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+
+    list_display = ('stadium', 'code')
+    list_filter = ('stadium',)
