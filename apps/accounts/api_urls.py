1e91e8f7dc35b0090c782431ee81af6442007a6d
Bamdad <bamdadmeh@yahoo.com>
2022-09-10 16:45:39 +0430

    app accounts : endpoints added

--- apps/accounts/api_urls.py
+++ apps/accounts/api_urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
 
+from apps.accounts.api_views import (
+
+    UserCreateAPIView,
+    UserLoginAPIView,
+    UserTokenRefreshView,
+    UserTokenVerifyView,
+)
+
 
 api_urls = [
 
+    path('', UserCreateAPIView.as_view(), name='api_register'),
+    path('login/', UserLoginAPIView.as_view(), name='api_login'),
+    path(
+        'token_refresh/',
+        UserTokenRefreshView.as_view(),
+        name='api_token_refresh'
+    ),
+    path(
+        'token_verify/',
+        UserTokenVerifyView.as_view(),
+        name='api_token_verify'
+    ),
 ]
