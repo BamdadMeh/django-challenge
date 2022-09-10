b139ef57d13dcbf130059cfda99a35e91404813a
Bamdad <bamdadmeh@yahoo.com>
2022-09-10 22:05:19 +0430

    TeamModel implemented

--- apps/team/models.py
+++ apps/team/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db.models import Model, CharField
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Team(Model):
+
+    name = CharField(_('name'), max_length=254, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('team')
+        verbose_name_plural = _('teams')
