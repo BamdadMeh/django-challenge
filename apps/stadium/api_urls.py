5b1b7258d43a8204f749e4e75cc95068c51ea3dc
Bamdad <bamdadmeh@yahoo.com>
2022-09-10 21:54:08 +0430

    app stadium : endpoints added

--- apps/stadium/api_urls.py
+++ apps/stadium/api_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from apps.stadium.api_views import StadiumCreateAPIView
+
 
 api_urls = [
 
-
+    path('', StadiumCreateAPIView.as_view(), name='api_create_stadium'),
 
 ]
