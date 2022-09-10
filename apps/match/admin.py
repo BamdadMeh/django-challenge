e3d15fafd15b8728faf17bbf264e2613d7567a66
Bamdad <bamdadmeh@yahoo.com>
2022-09-10 22:20:27 +0430

    MatchModel and MatchSeatInfoModel registered to Django admin

--- apps/match/admin.py
+++ apps/match/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from apps.match.models import Match, MatchSeatInfo
+
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+
+    list_display = ('host_team', 'guest_team', 'stadium', 'datetime')
+    list_filter = ('stadium',)
+
+
+@admin.register(MatchSeatInfo)
+class MatchSeatInfoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'match', 'seat', 'price', 'is_reserved', 'is_paid', 'buyer'
+    )
