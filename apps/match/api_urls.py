c3d3f5098fce8b718c5860fe6188bf4c73ff8eb1
Bamdad <bamdadmeh@yahoo.com>
2022-09-10 22:28:13 +0430

    app match : endpoints added

--- apps/match/api_urls.py
+++ apps/match/api_urls.py
@@ -1,7 +1,29 @@
 from django.urls import path
 
+from apps.match.api_views import (
+    MatchCreateAPIView,
+    MatchSeatInfoCreateAPIView,
+    ListCreateMatchSeatInfoAPIView,
+    ListUpdateMatchSeatInfoAPIView,
+)
+
 
 api_urls = [
 
-
+    path('', MatchCreateAPIView.as_view(), name='api_create_match'),
+    path(
+        'match_seat/',
+        MatchSeatInfoCreateAPIView.as_view(),
+        name='api_create_match_seat'
+    ),
+    path(
+        'match_seat_list/',
+        ListCreateMatchSeatInfoAPIView.as_view(),
+        name='api_create_list_match_seat'
+    ),
+    path(
+        'match_seat_list_booking/<int:match_id>/',
+        ListUpdateMatchSeatInfoAPIView.as_view(),
+        name='api_update_list_match_seat'
+    ),
 ]
